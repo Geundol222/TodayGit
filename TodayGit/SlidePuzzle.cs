472fb5146470d323457efbd9bff2fc1fa76d11ac
BuildTools <unconfigured@null.spigotmc.org>
2023-04-15 01:59:28 +0900

    feat : 슬라이드 퍼즐 난수 생성
    슬라이드 퍼즐을 구성할 난수를 생성하고 이차원배열에 저장

---
+++ TodayGit/SlidePuzzle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodayGit
+{
+    internal class SlidePuzzle
+    {
+        public int[,] RandomNumber()
+        {
+            Random rand = new Random();
+            int[] num = new int[25];
+            int[,] number = new int[5, 5];
+            int index = 0;
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                num[i] = rand.Next(0, 25);
+                for (int j = 0; j < i; j++)
+                {
+                    if (num[i] == num[j])
+                    {
+                        i--;
+                        break;
+                    }
+                }
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    number[i, j] = num[index++];
+                }
+            }
+            return number;
+        }
+    }
+}
