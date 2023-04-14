288c58784bd65b6f22016592eaa01be8da3a9309
현석 이 <inc02179@naver.com>
2023-04-14 19:50:25 +0900

    feat : Up & Down 게임
    컴퓨터가 랜덤으로 1~999의 값을 가지며, 플레이어가 10번 이내에 값을 맞추는 게임

--- TodayGit/Program.cs
+++ TodayGit/Program.cs
@@ -8,6 +8,7 @@
             int result = SumOfDigit(num);
             Console.WriteLine(result);
 
+            UpDownGame();
         }
 
         static void FindWord()
@@ -57,5 +58,83 @@
 
             return result;
         }
+
+        static void UpDownGame()
+        {
+            Random rand = new Random();
+
+            int com = rand.Next(0, 1000);
+            int count = 10;
+
+            while (true)
+            {
+                if (count == 0)
+                {
+                    Console.Clear();
+                    Console.WriteLine("도전횟수 초과! 패배했습니다.");
+                    Console.WriteLine("다시 하시겠습니까?");
+                    Console.WriteLine("[1] 예");
+                    Console.WriteLine("[2] 아니오");
+                    string restart = Console.ReadLine();
+
+                    if (restart == "1")
+                    {
+                        count = 10;
+                        com = rand.Next(0, 1000);
+                        continue;
+                    }
+                    else
+                        break;
+                }
+
+                Console.Clear();
+                Console.WriteLine($"도전횟수{count}");
+                Console.Write("숫자를 입력하세요(0 ~ 999) : ");
+                int input = int.Parse(Console.ReadLine());
+
+                if (input > 999 || input < 0)
+                {
+                    Console.WriteLine("error : 0에서 999사이의 값을 입력하세요");
+                    Console.WriteLine("계속하려면 아무키나 누르십시오");
+                    Console.ReadKey();
+                    continue;
+                }
+                else if (input > com)
+                {
+                    count--;
+                    Console.WriteLine("Down");
+                    Console.WriteLine("계속하려면 아무키나 누르십시오");
+                    Console.ReadKey();
+                    continue;
+                }
+                else if (input < com)
+                {
+                    count--;
+                    Console.WriteLine("Up");
+                    Console.WriteLine("계속하려면 아무키나 누르십시오");
+                    Console.ReadKey();
+                    continue;
+                }
+                else if (input == com)
+                {
+                    Console.WriteLine("정답입니다!");
+                    Console.WriteLine("다시 하시겠습니까?");
+                    Console.WriteLine("[1] 예");
+                    Console.WriteLine("[2] 아니오");
+                    string choice = Console.ReadLine();
+
+                    if (choice == "1")
+                    {
+                        count = 10;
+                        com = rand.Next(0, 1000);
+                        continue;
+                    }
+                    else
+                        break;
+                }
+
+            }
+        }
+
     }
 }
